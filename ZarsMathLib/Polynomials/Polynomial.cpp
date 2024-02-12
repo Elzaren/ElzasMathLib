217ac97f8a998bf4abf8e9f2ca34701abf49abe5
Elzaren <139815982+Elzaren@users.noreply.github.com>
2024-02-12 13:57:12 +0100

    Polynomial Class Added
    Not finished, only starting

---
+++ ZarsMathLib/Polynomials/Polynomial.cpp
@@ -0,0 +1,6 @@
+#include "Polynomial.h"
+
+int Polynomial::length(){
+    return m_length;
+}
+
--- ZarsMathLib/Polynomials/Polynomial.h
+++ ZarsMathLib/Polynomials/Polynomial.h
@@ -1,11 +1,19 @@
 #ifndef POLYNOMYAL_H_
 #define POLYNOMYAL_H_
 
-struct Polynomial
+struct term
 {
-
+  int coeficent;
+  int grade;
 };
 
+class Polynomial {
+  public:
+    Polynomial(int length, term terms[]);
+    int length(){};
 
-
+  private:
+    int m_length = 0;
+    int* m_terms = nullptr;
+};
 #endif
