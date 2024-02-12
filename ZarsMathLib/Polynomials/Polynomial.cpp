dee83b797d88b1f2328e9bf132d5c2c74c002a93
Elzaren <139815982+Elzaren@users.noreply.github.com>
2024-02-12 21:08:42 +0100

    Update to polynomial class
    Debating if using position to denote term degree or if using an struct with two ints will save memory when using long polynomials

--- ZarsMathLib/Polynomials/Polynomial.cpp
+++ ZarsMathLib/Polynomials/Polynomial.cpp
@@ -1,6 +1,28 @@
 #include "Polynomial.h"
 
+Polynomial::Polynomial(int length, int terms[]):m_terms(terms), m_length(length){}
+
 int Polynomial::length(){
     return m_length;
 }
 
+int Polynomial::getTerm(int grade){
+    return m_terms[grade];
+}
+
+void Polynomial::setTerm(int grade, int coeficent){
+    m_terms[grade]=coeficent;
+}
+
+Polynomial operator+(Polynomial &l, Polynomial &r){
+    Polynomial* biggerGrade = l.length() > r.length() ? &l : &r;
+    Polynomial outputPolynomial;
+    for(int c = 0; c < biggerGrade->length(); c++){
+        outputPolynomial.setTerm( c, l.getTerm(c) + r.getTerm(c));
+    }
+    return outputPolynomial;
+}
+
+Polynomial::~Polynomial(){
+    delete[] m_terms;
+}
--- ZarsMathLib/Polynomials/Polynomial.h
+++ ZarsMathLib/Polynomials/Polynomial.h
@@ -1,20 +1,19 @@
 #ifndef POLYNOMYAL_H_
 #define POLYNOMYAL_H_
 
-struct term
-{
-  int coeficent;
-  int grade;
-};
-
 class Polynomial {
   public:
-    Polynomial(int length, term terms[]);
-    int length(){};
+    Polynomial() = default;
+    Polynomial(int length, int terms[]);
+    ~Polynomial();
+    int length();
+    int getTerm(int term);
+    void setTerm(int grade, int coeficent);
     
 
   private:
     int m_length = 0;
     int* m_terms = nullptr;
 };
+
 #endif
