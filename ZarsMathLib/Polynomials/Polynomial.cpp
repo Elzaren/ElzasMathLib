ef2d992e12ca3f0d355d83c94756cd66aa2c684c
Elzaren <139815982+Elzaren@users.noreply.github.com>
2024-02-13 14:05:17 +0100

    save changes, nothings special
    Changed approach to having a term struct, still have to change some functions so they work with it

--- ZarsMathLib/Polynomials/Polynomial.cpp
+++ ZarsMathLib/Polynomials/Polynomial.cpp
@@ -1,24 +1,44 @@
 #include "Polynomial.h"
 
-Polynomial::Polynomial(int length, int terms[]):m_terms(terms), m_length(length){}
+Polynomial::Polynomial(int length, Term terms[]):m_terms(terms), m_length(length){}
 
 int Polynomial::length(){
     return m_length;
 }
 
-int Polynomial::getTerm(int grade){
-    return m_terms[grade];
+void Polynomial::addTerm(int grade, int coeficent){
+    for(int c = 0; c <= m_length; c++){
+        
+    }
+}
+
+
+Term Polynomial::getTerm(int grade){
+    for(int c = 0; c <= m_length; c++){
+        if(m_terms[c].grade==grade){
+            return m_terms[c];
+        }
+    }
 }
 
 void Polynomial::setTerm(int grade, int coeficent){
-    m_terms[grade]=coeficent;
+
+   int c = 0;
+
+   do{
+    c++;
+   }while( (c!=m_terms[c].grade) && (c<=m_length) );
+
+    if(m_terms[c].grade==grade){ m_terms[c].coeficent = coeficent; }
+    else{ addTerm(grade, coeficent); }
 }
 
+//Change cuz u made a term struct and grade isnt pos in array anymore
 Polynomial operator+(Polynomial &l, Polynomial &r){
     Polynomial* biggerGrade = l.length() > r.length() ? &l : &r;
     Polynomial outputPolynomial;
     for(int c = 0; c < biggerGrade->length(); c++){
-        outputPolynomial.setTerm( c, l.getTerm(c) + r.getTerm(c));
+        outputPolynomial.setTerm( c, l.getTerm(c).grade + r.getTerm(c).grade);
     }
     return outputPolynomial;
 }
--- ZarsMathLib/Polynomials/Polynomial.h
+++ ZarsMathLib/Polynomials/Polynomial.h
@@ -1,19 +1,26 @@
 #ifndef POLYNOMYAL_H_
 #define POLYNOMYAL_H_
 
+struct Term{
+  int coeficent = 0;
+  int grade = 0;
+};
+
+
 class Polynomial {
   public:
     Polynomial() = default;
-    Polynomial(int length, int terms[]);
+    Polynomial(int length, Term terms[]);
     ~Polynomial();
     int length();
-    int getTerm(int term);
+    void addTerm(int grade, int coeficent);
+    Term getTerm(int grade);
     void setTerm(int grade, int coeficent);
-    
 
   private:
     int m_length = 0;
-    int* m_terms = nullptr;
+    Term* m_terms = nullptr;
+    int m_grade = 0;
 };
 
 #endif
