b8b443d2f0769eb5c3ae259cc5014cb60c23808b
Elzaren <139815982+Elzaren@users.noreply.github.com>
2024-02-19 13:29:56 +0100

    No fricking IDE debugs this, sth wrong, I can feel it

--- ZarsMathLib/Polynomials/Polynomial.cpp
+++ ZarsMathLib/Polynomials/Polynomial.cpp
@@ -1,64 +1,11 @@
 #include "Polynomial.h"
 #include <iostream>
 
-Polynomial::Polynomial(int length, Term terms[]):m_terms(terms), m_length(length){}
+Polynomial::~Polynomial(){
+    delete[] m_terms;
+    m_terms = nullptr;
+}
 
 int Polynomial::length(){
     return m_length;
-}
-
-
-void Polynomial::addTerm(int grade, int coeficent){
-    int c = -1;
-    do{
-        c++;
-    }while( (c <= m_length) && (m_terms[c].grade!=grade) );
-    if(m_terms[c].grade==grade){return; }
-    else{
-
-        Term* newTerms = new Term[m_length+1];
-        for(int c = 0; c<m_length; c++){
-            newTerms[c] = m_terms[c];
-        }
-        newTerms[m_length].grade = grade;
-        newTerms[m_length].grade = coeficent;
-        m_length+=1;
-        delete[] m_terms;
-        m_terms = newTerms;
-    }
-}
-
-
-Term Polynomial::getTerm(int grade){
-    for(int c = 0; c <m_length; c++){
-        if(m_terms[c].grade==grade){
-            return m_terms[c];
-        }
-    }
-}
-
-void Polynomial::setTerm(int grade, int coeficent){
-
-   int c = -1;
-
-   do{
-    c++;
-   }while( (c!=m_terms[c].grade) && (c<=m_length) );
-
-    if(m_terms[c].grade==grade){ m_terms[c].coeficent = coeficent; }
-    else{ addTerm(grade, coeficent); }
-}
-
-//Change cuz u made a term struct and grade isnt pos in array anymore
-Polynomial operator+(Polynomial &l, Polynomial &r){
-    Polynomial* biggerGrade = l.length() > r.length() ? &l : &r;
-    Polynomial outputPolynomial;
-    for(int c = 0; c < biggerGrade->length(); c++){
-        outputPolynomial.setTerm( c, l.getTerm(c).grade + r.getTerm(c).grade);
-    }
-    return outputPolynomial;
-}
-
-Polynomial::~Polynomial(){
-    delete[] m_terms;
 }
--- ZarsMathLib/Polynomials/Polynomial.h
+++ ZarsMathLib/Polynomials/Polynomial.h
@@ -1,5 +1,5 @@
-#ifndef POLYNOMYAL_H_
-#define POLYNOMYAL_H_
+#ifndef POLYNOMIAL_H_
+#define POLYNOMIAL_H_
 
 struct Term{
   int coeficent = 0;
@@ -10,12 +10,9 @@ struct Term{
 class Polynomial {
   public:
     Polynomial() = default;
-    Polynomial(int length, Term terms[]);
     ~Polynomial();
+
     int length();
-    void addTerm(int grade, int coeficent);
-    Term getTerm(int grade);
-    void setTerm(int grade, int coeficent);
 
   private:
     int m_length = 0;
