f5c6a0a765edde084a31f651e72da908f5d12500
Elzaren <139815982+Elzaren@users.noreply.github.com>
2024-05-11 17:55:29 +0200

    Added cross and dot products to Vector3

--- Vector/Vector3.cpp
+++ Vector/Vector3.cpp
@@ -1,21 +1,27 @@
-//
-// Created by Elzaren on 01/12/2023.
-//
-
 #include "Vector3.h"
 
-Vector3 operator+(const Vector3 &l, const Vector3 &r){
-    return {l.x+r.x, l.y+r.y, l.z+r.z};
+
+Vector3 operator+(const Vector3 &l, const Vector3 &r) {
+    return {l.x + r.x, l.y + r.y, l.z + r.z};
 }
 
-Vector3 operator-(const Vector3 &l, const Vector3 &r){
-    return {l.x-r.x, l.y-r.y, l.z-r.z};
+Vector3 operator-(const Vector3 &l, const Vector3 &r) {
+    return {l.x - r.x, l.y - r.y, l.z - r.z};
 }
 
-Vector3 operator*(const Vector3 &l, float f){
-    return {l.x*f, l.y*f, l.z*f};
+Vector3 operator*(const Vector3 &l, float f) {
+    return {l.x * f, l.y * f, l.z * f};
 }
 
-Vector3 operator*(const Vector3 &l, Vector3 &r){
-    return {l.x*r.x, l.y*r.y, l.z*r.z};
+float dot(const Vector3 &l, const Vector3 &r) {
+    return l.x * r.x + l.y * r.y + l.z * r.z;
+}
+
+Vector3 cross(const Vector3 &l, const Vector3 &r) {
+    return {l.y * r.z - (l.z * r.y), l.z * r.x - (l.x * r.z), l.x * r.y - (l.y * r.x)};
+}
+
+std::ostream &operator<<(std::ostream &os, const Vector3 &v) {
+    os << '(' << v.x << ',' << v.y << ',' << v.z << ')';
+    return os;
 }
--- Vector/Vector3.h
+++ Vector/Vector3.h
@@ -1,6 +1,9 @@
+
+
 #ifndef VECTOR3_H_
 #define VECTOR3_H_
 
+#include "ostream"
 
 struct Vector3 {
     union {
@@ -9,17 +12,32 @@ struct Vector3 {
         };
         float v[3];
     };
-    inline Vector3() : x(0.0f), y(0.0f), z(0.0f) { }
-    inline Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) { }
-    inline Vector3(float *fv) : x(fv[0]), y(fv[1]), z(fv[2]) { }
+
+    inline Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
+
+    inline Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+
+    inline Vector3(float *fv) : x(fv[0]), y(fv[1]), z(fv[2]) {}
 };
 
-#endif //VECTOR3_H_
 
-#define VECTOR3_EPSILON 0.000001f
 
 //define basic operators
 Vector3 operator+(const Vector3 &l, const Vector3 &r);
+
 Vector3 operator-(const Vector3 &l, const Vector3 &r);
+
 Vector3 operator*(const Vector3 &l, float f);
-Vector3 operator*(const Vector3 &l, Vector3 &r);
+
+float dot(const Vector3 &l, const Vector3 &r);
+
+Vector3 cross(const Vector3 &l, const Vector3 &r);
+
+std::ostream &operator<<(std::ostream &os, const Vector3 &v);
+
+#define VECTOR3_EPSILON 0.000001f
+
+#endif //VECTOR3_H_
+
+
+
